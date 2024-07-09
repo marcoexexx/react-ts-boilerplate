87431452e8ffe3119e73d8ada7a6688080230e0a
marco <toyko2001@gmail.com>
2024-07-09 10:40:25 +0630

    fix localeAction payload type.

--- apps/web/src/context/locale/types.ts
+++ apps/web/src/context/locale/types.ts
@@ -5,7 +5,7 @@ type LocaleStore = {} & import("locale").Locale;
 
 interface SetLocaleAction {
   type: ActionType<"@@LOCALE", "SET_LOCALE">;
-  payload: TodoFilterInput;
+  payload: import("locale").Locale;
 }
 
 type LocaleAction = SetLocaleAction;
